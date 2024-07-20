4b49d4fe4f534b041f8d114d8da864ec2bd64274
Nanase <nanase@users.noreply.github.com>
2024-07-21 08:08:18 +0900

    Add ImpulseResponse.calcImpulseResponse method

--- src/lib/filter/impulseResponse.ts
+++ src/lib/filter/impulseResponse.ts
@@ -2,33 +2,41 @@ import type { Coefficients } from './coefficients';
 
 export class ImpulseResponse {
   static getImpulseResponse(coefficients: Coefficients, length: number) {
-    let fiveParamsCoefficients: number[] = [];
+    let normalizedCoefficients: number[] = [];
 
     if (coefficients.length === 6) {
-      fiveParamsCoefficients = coefficients.normalizeToFiveParameters();
+      normalizedCoefficients = coefficients.normalizeToFiveParameters();
     } else if (coefficients.length === 5) {
-      fiveParamsCoefficients = coefficients;
+      normalizedCoefficients = coefficients;
     } else {
       throw new Error('Invalid coefficients');
     }
 
     const impulses = new Float64Array(length);
+    ImpulseResponse.calcImpulseResponse(impulses, normalizedCoefficients);
+
+    return impulses;
+  }
+
+  static calcImpulseResponse(buffer: Float64Array, coefficients: number[]): void {
+    if (coefficients.length !== 5) {
+      throw new Error('Coefficients must to be normalized.');
+    }
+
     let [o1, o2, i0, i1, i2] = [0.0, 0.0, 1.0, 0.0, 0.0];
 
-    for (let i = 0; i < length; i++) {
-      impulses[i] =
-        fiveParamsCoefficients[0] * i0 +
-        fiveParamsCoefficients[1] * i1 +
-        fiveParamsCoefficients[2] * i2 +
-        fiveParamsCoefficients[3] * o1 +
-        fiveParamsCoefficients[4] * o2;
+    for (let i = 0; i < buffer.length; i++) {
+      buffer[i] =
+        coefficients[0] * i0 +
+        coefficients[1] * i1 +
+        coefficients[2] * i2 +
+        coefficients[3] * o1 +
+        coefficients[4] * o2;
       i2 = i1;
       i1 = i0;
       o2 = o1;
-      o1 = impulses[i];
+      o1 = buffer[i];
       i0 = 0.0;
     }
-
-    return impulses;
   }
 }
