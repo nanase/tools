f92a84743b64b1bcb86797a35bc5f54f58161a5a
Nanase <nanase@users.noreply.github.com>
2024-07-30 07:31:52 +0900

    Fix MathJax's typeset process

--- src/components/common/MathJax.vue
+++ src/components/common/MathJax.vue
@@ -1,11 +1,33 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
-onMounted(() => {
-  (window as any).MathJax.typeset();
+const { tag = 'span', block } = defineProps<{
+  tag?: string;
+  block?: boolean;
+}>();
+const Tag = tag;
+
+const container = ref<HTMLElement>();
+
+onMounted(async () => {
+  if (container.value) {
+    if (container.value.children.length === 0) {
+      if (block) {
+        container.value.innerText = `$$ ${container.value.innerText} $$`;
+      } else {
+        container.value.innerText = `\\( ${container.value.innerText} \\)`;
+      }
+    }
+
+    await (window as any).MathJax.startup.promise;
+    (window as any).MathJax.typesetClear([container.value]);
+    await (window as any).MathJax.typesetPromise([container.value]);
+  }
 });
 </script>
 
 <template>
-  <slot></slot>
+  <Tag ref="container">
+    <slot></slot>
+  </Tag>
 </template>
