58383cd6bb85efe5087747c4c18c9105fa2c506f
Nanase <nanase@users.noreply.github.com>
2024-09-04 19:35:11 +0900

    Add name and description propeties in page.ts

--- src/page.ts
+++ src/page.ts
@@ -1,68 +1,81 @@
 export interface Page {
   id: string;
   title: string;
+  description?: string;
   path?: string;
   icon?: string;
 }
 
 export interface PageSection {
   id: string;
+  name: string;
   pages: Page[];
 }
 
 export const PageList: PageSection[] = [
   {
     id: 'control',
+    name: '制御',
     pages: [
       {
         id: 'control/pid',
         title: 'PID制御',
         icon: 'mdi-ship-wheel',
+        description: 'フィードバック制御の一種であるPID制御を各項を調整しながらシミュレートします。',
       },
     ],
   },
   {
     id: 'electric',
+    name: '電子回路',
     pages: [
       {
         id: 'electric/combination',
         title: '受動素子 組み合わせ',
         icon: 'mdi-resistor',
+        description: '抵抗器、コンデンサ、インダクタについて、任意の値に近似できる組み合わせを探索します。',
       },
       {
         id: 'electric/timer555',
         title: 'タイマIC 555',
         icon: 'mdi-chip',
+        description: '抵抗器とコンデンサによってタイマICの出力の変化をシミュレートします。',
       },
     ],
   },
   {
     id: 'filter',
+    name: 'フィルタ回路',
     pages: [
       {
         id: 'filter/biquad',
         title: '双2次フィルタ',
         icon: 'mdi-filter',
+        description: '各種フィルタ特性に対応する双2次フィルタ（バイクアッドフィルタ）の係数を計算します。',
       },
     ],
   },
   {
     id: 'jjy',
+    name: 'JJY（標準電波）',
     pages: [
       {
         id: 'jjy/simulator',
         title: 'JJY シミュレータ',
         icon: 'mdi-alarm',
+        description: '電波時計などで利用されているJJYの信号をシミュレートします。',
       },
     ],
   },
   {
     id: 'svg',
+    name: 'SVG',
     pages: [
       {
         id: 'svg/theme-checker',
         title: 'SVG テーマスキーマチェッカー',
         icon: 'mdi-theme-light-dark',
+        description: 'ライトテーマとダークテーマによって、SVGの外観が意図したものになるかを確認できます。',
       },
     ],
   },
