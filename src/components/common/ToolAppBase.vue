4bea677f09fd35c4a27bbe8153874009958e0ee4
Nanase <nanase@users.noreply.github.com>
2024-09-11 21:00:33 +0900

    Fix slot inheritance in ToolAppBase.vue

--- src/components/common/ToolAppBase.vue
+++ src/components/common/ToolAppBase.vue
@@ -14,12 +14,40 @@ const drawerOpened = ref<boolean>();
 <template>
   <AppBase :toolbarTitle flat floating color="primary" density="compact">
     <template #mainPrepend>
-      <NavigationDrawer v-model:opened="drawerOpened" :pageId />
+      <slot name="mainPrepend"><NavigationDrawer v-model:opened="drawerOpened" :pageId /></slot>
     </template>
     <template #appbarPrepend>
-      <v-app-bar-nav-icon variant="text" @click.stop="drawerOpened = !drawerOpened" aria-label="ナビゲーションを表示" />
+      <slot name="appbarPrepend">
+        <v-app-bar-nav-icon
+          variant="text"
+          @click.stop="drawerOpened = !drawerOpened"
+          aria-label="ナビゲーションを表示"
+        />
+      </slot>
+    </template>
+    <template #errorSnackbar>
+      <slot name="errorSnackbar"></slot>
+    </template>
+    <template #appbarAppend>
+      <slot name="appbarAppend"></slot>
+    </template>
+    <template #toolbarPrepend>
+      <slot name="toolbarPrepend"></slot>
+    </template>
+    <template #toolbarAppend>
+      <slot name="toolbarAppend"></slot>
+    </template>
+    <template #header>
+      <slot name="header"></slot>
     </template>
 
     <slot></slot>
+
+    <template #footer>
+      <slot name="footer"></slot>
+    </template>
+    <template #mainAppend>
+      <slot name="mainAppend"></slot>
+    </template>
   </AppBase>
 </template>
