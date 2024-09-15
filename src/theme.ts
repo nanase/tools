5f9c61aa1175f75531c93a99ea36119cf11b7164
Nanase <nanase@users.noreply.github.com>
2024-09-15 11:47:32 +0900

    Update theme

--- src/theme.ts
+++ src/theme.ts
@@ -5,11 +5,25 @@ const light: ThemeDefinition = {
   dark: false,
   colors: {
     background: '#FFFFFF',
+    surface: '#ffffff',
     containerBackground: '#FFFFFF',
     primary: colors.indigo.base,
     secondary: colors.indigo.base,
     positive: colors.green.darken3,
     negative: colors.red.darken3,
+    //
+    v2DrawerBackground: '#f8f8f8',
+    v2DrawerList: '#3c3c43',
+    v2DrawerListActive: colors.indigo.base,
+    v2AppBarBackground: '#ffffff',
+    v2SnackbarBackground: '#171717',
+  },
+  variables: {
+    'theme-on-surface': '#3c3c43',
+    'theme-on-background': '#3c3c43',
+    'high-emphasis-opacity': '1.0',
+    'v2-content-color': '#3c3c43',
+    'v2-snackbar-border': 'none',
   },
 };
 
@@ -17,11 +31,25 @@ const dark: ThemeDefinition = {
   dark: true,
   colors: {
     background: '#121212',
+    surface: '#171717',
     containerBackground: '#121212',
     primary: '#262852',
     secondary: colors.indigo.darken4,
     positive: colors.green.lighten2,
     negative: colors.red.lighten1,
+    //
+    v2DrawerBackground: '#171717',
+    v2DrawerList: '#bcbcbc',
+    v2DrawerListActive: colors.blue.lighten2,
+    v2AppBarBackground: '#171717',
+    v2SnackbarBackground: '#171717',
+  },
+  variables: {
+    'theme-on-surface': 'rgba(255, 255, 245, .86)',
+    'theme-on-background': 'rgba(255, 255, 245, .86)',
+    'high-emphasis-opacity': '0.86',
+    'v2-content-color': 'rgba(255, 255, 245, .86)',
+    'v2-snackbar-border': '1px solid #3c3c43',
   },
 };
 
